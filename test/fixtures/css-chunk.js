3013752ccfb73c5879c2d3e5248d3ad4e9af3ea4
kpdecker <kpdecker@gmail.com>
2014-09-24 23:26:07 -0500

    Implement css chunk loader
    Creates require.css loader which extracts all css content in the javascript chunk into a standalone file.
    
    Fixes a portion of #3

---
+++ test/fixtures/css-chunk.js
@@ -0,0 +1,4 @@
+require.css('./css1.css');
+require.css('./css2.css');
+
+require('./router1');
---
+++ test/plugins/css-chunk.js
@@ -0,0 +1,115 @@
+var CssChunkPlugin = require('../../lib/plugins/css-chunk'),
+    ModuleNotFoundError = require('webpack/lib/ModuleNotFoundError'),
+    webpack = require('webpack');
+
+var expect = require('chai').expect,
+    temp = require('temp'),
+    fs = require('fs'),
+    path = require('path');
+
+describe('css chunk plugin', function() {
+  var outputDir;
+
+  beforeEach(function(done) {
+    temp.mkdir('loader-plugin', function(err, dirPath) {
+      if (err) {
+        throw err;
+      }
+
+      outputDir = dirPath;
+      done();
+    });
+  });
+  afterEach(function() {
+    temp.cleanupSync();
+  });
+
+  it('should merge css modules into a single file per-chunk', function(done) {
+    var cssChunkPlugin = new CssChunkPlugin(),
+        entry = path.resolve(__dirname + '/../fixtures/css-chunk.js');
+
+    webpack({
+      entry: entry,
+      output: {path: outputDir},
+
+      plugins: [
+        cssChunkPlugin
+      ]
+    }, function(err, status) {
+      expect(err).to.not.exist;
+      expect(status.compilation.errors).to.be.empty;
+      expect(status.compilation.warnings).to.be.empty;
+
+      // Verify the loader boilerplate
+      var output = fs.readFileSync(outputDir + '/bundle.js').toString();
+      expect(output).to.not.match(/\.foo/);
+      expect(output).to.not.match(/\.baz/);
+
+      // Verify the file records
+      expect(Object.keys(status.compilation.assets)).to.eql(['bundle.js', 'bundle.css']);
+
+      // Verify the actual css content
+      output = fs.readFileSync(outputDir + '/bundle.css').toString();
+      expect(output).to.match(/\.foo/);
+      expect(output).to.match(/\.baz/);
+
+      done();
+    });
+  });
+
+  it('should nop without css content', function(done) {
+    var cssChunkPlugin = new CssChunkPlugin(),
+        entry = path.resolve(__dirname + '/../fixtures/router1.js');
+
+    webpack({
+      entry: entry,
+      output: {path: outputDir},
+
+      plugins: [
+        cssChunkPlugin
+      ]
+    }, function(err, status) {
+      expect(err).to.not.exist;
+      expect(status.compilation.errors).to.be.empty;
+      expect(status.compilation.warnings).to.be.empty;
+
+      // Verify the loader boilerplate
+      var output = fs.readFileSync(outputDir + '/bundle.js').toString();
+      expect(output).to.not.match(/\.foo/);
+      expect(output).to.not.match(/\.baz/);
+
+      // Verify the file records
+      expect(Object.keys(status.compilation.assets)).to.eql(['bundle.js']);
+
+      done();
+    });
+  });
+
+  it('should handled erroring dependencies', function(done) {
+    var cssChunkPlugin = new CssChunkPlugin(),
+        entry = path.resolve(__dirname + '/../fixtures/css-missing.js');
+
+    webpack({
+      entry: entry,
+      output: {path: outputDir},
+
+      plugins: [
+        cssChunkPlugin
+      ]
+    }, function(err, status) {
+      expect(err).to.not.exist;
+      expect(status.compilation.errors[0]).to.be.instanceof(ModuleNotFoundError);
+      expect(status.compilation.warnings).to.be.empty;
+
+      // Verify the loader boilerplate
+      var output = fs.readFileSync(outputDir + '/bundle.js').toString();
+      expect(output).to.not.match(/\.foo/);
+      expect(output).to.not.match(/\.baz/);
+
+      // Verify the file records
+      expect(Object.keys(status.compilation.assets)).to.eql(['bundle.js']);
+
+      done();
+    });
+  });
+});
