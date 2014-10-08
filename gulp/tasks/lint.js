8ed1ecce25bf66285767ba01900fd014c90119ec
Eric Baer <me@ericbaer.com>
2014-10-08 11:52:44 -0700

    Add jscs to the build

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var config = require('../config');
 
 var plumber = require('gulp-plumber');
+var jscs = require('gulp-jscs');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var handleError = require('../util/handle-errors');
@@ -16,4 +17,13 @@ gulp.task('jshint', function() {
     .on('error', handleError);
 });
 
-gulp.task('lint', ['jshint']);
+gulp.task('jscs', function () {
+  var allJavascript = Array.prototype.concat(config.source, config.gulp, config.tests);
+
+  return gulp.src(allJavascript)
+    .pipe(plumber())
+    .pipe(jscs())
+    .on('error', handleError);
+});
+
+gulp.task('lint', ['jshint', 'jscs']);
