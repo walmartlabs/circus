4f91b1f9071c2b28732917e9896c79ad8040fd0d
kpdecker <kpdecker@gmail.com>
2014-11-20 16:36:57 -0600

    Add dev watch task

--- gulpfile.js
+++ gulpfile.js
@@ -7,10 +7,15 @@
   To add a new task, simply add a new task file to ./build/tasks.
 */
 
-var gulp = require('gulp');
-var requireDir = require('require-dir');
+var gulp = require('gulp'),
+    requireDir = require('require-dir'),
+    config = require('./gulp/config');
 
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./gulp/tasks', { recurse: true });
 
 gulp.task('default', ['lint']);
+
+gulp.task('dev', ['lint', 'test'], function() {
+  gulp.watch(config.source.concat(['./test/**/*.js']), ['lint', 'test']);
+});
