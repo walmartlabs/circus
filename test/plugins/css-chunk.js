30ac066ab8404ac4397f23f756dfb388823117e7
kpdecker <kpdecker@gmail.com>
2015-01-12 16:57:56 -0600

    Handle published assets within mixins
    Fixes walmartlabs/circus-stylus#4

--- test/plugins/css-chunk.js
+++ test/plugins/css-chunk.js
@@ -1,4 +1,5 @@
 var CssChunkPlugin = require('../../lib/plugins/css-chunk'),
+    Circus = require('../../'),
     ModuleNotFoundError = require('webpack/lib/ModuleNotFoundError'),
     webpack = require('webpack');
 
@@ -94,6 +95,76 @@ describe('css chunk plugin', function() {
       expect(output).to.match(/background: url\(data:foo\);/);
       expect(output).to.match(/background: url\(e46d046421eba561b2d062319480f69a.gif#foo\);/);
       expect(output).to.match(/background: url\(e46d046421eba561b2d062319480f69a.gif\?#foo\);/);
+      expect(output).to.match(/background: url\(#foo\);/);
+      expect(output).to.not.match(/undefined/);
+
+      done();
+    });
+  });
+
+  it('should import published urls from components', function(done) {
+    var entry = path.resolve(__dirname + '/../fixtures/css-image.js');
+
+    webpack(Circus.config({
+      entry: entry,
+
+      components: {
+        zeus: {
+          modules: {
+            0: {
+              chunk: 0,
+              name: 'foo'
+            },
+            1: {
+              chunk: 0,
+              name: 'handlebars/runtime'
+            }
+          },
+          published: {'e46d046421eba561b2d062319480f69a.gif': 'e46d046421eba561b2d062319480f69a.foo'},
+          entry: 'bundle.js'
+        }
+      },
+      output: {
+        path: outputDir
+      },
+
+      module: {
+        loaders: [
+          {test: /\.gif$/, loader: 'file'}
+        ]
+      },
+
+      resolve: {
+        modulesDirectories: [
+          outputDir
+        ]
+      }
+    }), function(err, status) {
+      expect(err).to.not.exist;
+      expect(status.compilation.errors).to.be.empty;
+      expect(status.compilation.warnings).to.be.empty;
+
+      // Verify the loader boilerplate
+      var output = fs.readFileSync(outputDir + '/bundle.js').toString();
+      expect(output).to.not.match(/\.foo/);
+      expect(output).to.not.match(/\.baz/);
+
+      // Verify the file records
+      expect(Object.keys(status.compilation.assets).sort()).to.eql([
+        '0.bundle.css',
+        'bundle.js',
+        'bundle.js.map',
+        'circus.json',
+      ]);
+
+      // Verify the actual css content
+      output = fs.readFileSync(outputDir + '/0.bundle.css').toString();
+      expect(output).to.match(/\.foo/);
+      expect(output).to.match(/\.baz/);
+      expect(output).to.match(/background: url\(e46d046421eba561b2d062319480f69a.foo\);/);
+      expect(output).to.match(/background: url\(data:foo\);/);
+      expect(output).to.match(/background: url\(e46d046421eba561b2d062319480f69a.foo#foo\);/);
+      expect(output).to.match(/background: url\(e46d046421eba561b2d062319480f69a.foo\?#foo\);/);
       expect(output).to.match(/background: url\(#foo\);/);
       expect(output).to.not.match(/undefined/);
 
