e0f29c77c44b9c72c9e3cebd59c82b2465ab346a
kpdecker <kpdecker@gmail.com>
2015-01-31 17:06:27 -0600

    Fix external name resolution for direct main refs

---
+++ test/fixtures/main-packages.js
@@ -0,0 +1,9 @@
+var _ = require('underscore/underscore'),
+    Handlebars = require('handlebars/runtime'),
+    Bang = require('!./bang');
+
+var log = document.createElement('log');
+log.info = '_: ' + (!!_) + ' Handlebars: ' + (!!Handlebars);
+document.body.appendChild(log);
+
+console.log('DONE');
